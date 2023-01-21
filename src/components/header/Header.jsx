321ccb2358494c48a76a1940ce65022dae20f50b
yashsaini7060 <yk18052000@gmail.com>
2023-01-21 21:20:37 +0530

    name colour changed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
   return (
     <header className="header">
       <nav className="nav container">
-        <a href="index.html" className="nav_-logo">Yash</a>
+        <a href="index.html" className="nav__logo">Yash</a>
         <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
           <ul className="nav__list grid">
             <li className="nav__item">
