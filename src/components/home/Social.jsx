9d5e3de1b171c0e4f97ea8e514ec41f9c414a598
yashsaini7060 <yk18052000@gmail.com>
2023-01-21 15:53:55 +0530

    added home secion

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a href="https://www.instagram.com/yash__saini_/" className="home__social-icon" target="_blank">
+      <i className="uil uil-instagram"></i>
+      </a>
+      <a href="https://dribbble.com/" className="home__social-icon" target="_blank">
+      <i className="uil uil-dribbble"></i>
+      </a>
+      <a href="https://github.com/yashsaini7060" className="home__social-icon" target="_blank">
+      <i className="uil uil-github-alt"></i>
+      </a>
+    </div>
+  )
+}
+
+export default Social
