5e139d3329d324ca750cc5f3f5e7de2698077282
yashsaini7060 <yk18052000@gmail.com>
2023-01-24 00:35:46 +0530

    added scroll up and header highlights

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -4,7 +4,7 @@ import Frontend from './Frontend'
 import Backend from './Backend'
 const Skills = () => {
   return (
-    <section className="skills section" id="id">
+    <section className="skills section" id="skills">
       <h2 className="section__title">Skills</h2>
       <span className="section__subtitle">My technical level</span>
 
